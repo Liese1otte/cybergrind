38bd0443a9dcf9ef8878d154f1aad0aaa68d18cb
Lotti <whimsy.lieselotte@gmail.com>
2023-08-13 18:16:30 +0400

    Refactoring overhaul part 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,19 @@
 import { Canvas } from '@threlte/core';
 
 import InputMenu from '$lib/InputMenu.svelte';
-import MainScene from '$src/lib/MainScene.svelte';
+import MainScene from '$lib/MainScene.svelte';
+
+import { base } from '$app/paths';
 </script>
 
-<div class="wrapper">
-	<div class="canvas">
+<svelte:head>
+	<title>Cybegrind 3D Editor</title>
+	<link rel="icon" type="image/x-icon" href="{base}/cube.png" />
+</svelte:head>
+
+<!-- mirroring fourth then switching maps then mirroring fourth again doesn't work lol -->
+<div class="website">
+	<div class="canvas-container">
 		<Canvas
 			toneMapping={0}
 			colorSpace={'srgb-linear'}
@@ -17,22 +25,32 @@ import MainScene from '$src/lib/MainScene.svelte';
 			<MainScene />
 		</Canvas>
 	</div>
-	<div class="menu">
+	<div class="menu-container">
 		<InputMenu />
 	</div>
 </div>
 
 <style>
 @import '/src/styles/globals.css';
-.wrapper {
+
+div.website {
+	display: grid;
+	grid-template-rows: 1fr;
+	grid-template-columns: 1fr 1fr 1fr;
+
 	width: 100%;
 	height: 100%;
-	display: flex;
 }
 
-.canvas {
-	flex: 1;
-	background: radial-gradient(#282828, #171717);
-	background-attachment: fixed;
+div.canvas-container {
+	grid-column: 1 / span 2;
+
+	margin: 20px 10px 20px 20px;
+}
+
+div.menu-container {
+	grid-column: 3 / span 1;
+
+	margin: 20px 20px 20px 10px;
 }
 </style>
